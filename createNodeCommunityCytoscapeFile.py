2f423d59b2e00b4f04b856af3de232c457685551
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2020-12-09 14:40:07 +0100

    Add files via upload
    Create Node attributes 'community' for Cytoscape

---
+++ createNodeCommunityCytoscapeFile.py
@@ -0,0 +1,24 @@
+# file to import community files and create one file for Cytoscape
+# text file contains Node and Community
+
+# import libraries
+import os
+import pandas as pd
+# Set the directory of the files
+pathToCommunities="E:/Phd Thesis/Chapter 4 subsection EGM/EGM/analyse paper/Genomic interaction network/Genomic interaction network files/Communities total network/"
+os.chdir(pathToCommunities)
+
+# list the files in the directory
+filesCommunity=os.listdir()
+
+indexCom=0
+# Create the data frame by reading all the files and storing them
+for x in filesCommunity:
+    Community_tmp=pd.read_csv(x,sep=",")
+    Community_tmp=Community_tmp.assign(Community=indexCom)
+    if indexCom==0:
+        NodeCommunity=Community_tmp
+        indexCom=indexCom+1
+    else:
+        NodeCommunity=pd.concat([NodeCommunity,Community_tmp])
+        indexCom=indexCom+1
